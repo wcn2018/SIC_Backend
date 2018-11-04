af0416a47d8ba73d58cc7e5157e20774487a7f67
William <wcn2018@gmail.com>
2018-11-03 21:25:14 -0400

    Fixed the repeats errora nd added followers/following lists

--- src/main/java/com/twitAPI/Twitterer.java
+++ src/main/java/com/twitAPI/Twitterer.java
@@ -1,12 +1,6 @@
 package com.twitAPI;
-import twitter4j.GeoLocation;       // jar found at http://twitter4j.org/en/index.html
-import twitter4j.Paging;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.TwitterException;
+
+import twitter4j.*;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -14,58 +8,76 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Twitterer
-{
+public class Twitterer {
     private Twitter twitter;
     private PrintStream consolePrint;
     private List<Status> statuses;
+    private List<IDs> follows;
+    private List<IDs> followers;
     //private List<Status> retweets;
 
 
-    public Twitterer(PrintStream console)
-    {
+    public Twitterer(PrintStream console) {
         // Makes an instance of Twitter - this is re-useable and thread safe.
         // Connects to Twitter and performs authorizations.
         twitter = TwitterFactory.getSingleton();
         consolePrint = console;
         statuses = new ArrayList<Status>();
+        follows = new ArrayList<IDs>();
+        followers = new ArrayList<IDs>();
+
         //retweets = new ArrayList<Status>();
     }
 
     /******************  Adapted Tweet Grabber *******************/
     /**
      * This method queries the tweets of a particular user's handle.
-     * @param *String  the Twitter handle (username) without the @sign
-     * Passes to arrays containing plain statuses and retweets (optional)
+     *
+     * @param *String the Twitter handle (username) without the @sign
+     *                Passes to arrays containing plain statuses and retweets (optional)
      */
     @SuppressWarnings("unchecked")
-    public void queryHandle(String handle) throws TwitterException, IOException
-    {
+    public void queryHandle(String handle) throws TwitterException, IOException {
         statuses.clear();// queryHandle will be looped, clear statuses each time or else it's a mess
+        follows.clear();//same
+        followers.clear();
+
         getTweets(handle);
         int numTweets = statuses.size();
-        while (numTweets > 0){
+        while (numTweets > 0) {
             numTweets--;
-            System.out.println("Tweet"+numTweets+": "+statuses.get(numTweets).getText());
+            System.out.println("Tweet" + numTweets + ": " + statuses.get(numTweets).getText());
         }
+
+        getNetwork(handle);//creates arraylist of follows and followers
+
     }
+
 
     /**
      * This helper method fetches the most recent 2,000 tweets of a particular user's handle and
      * stores them in an arrayList of Status objects.  Populates statuses.
-     * @param *String  the Twitter handle (username) without the @sign
+     *
+     * @param *String the Twitter handle (username) without the @sign
      */
-    private void getTweets(String handle) throws TwitterException, IOException
-    {
+    private void getTweets(String handle) throws TwitterException, IOException {
         //total tweets grabbed = p*count in the Paging instance
-        Paging page = new Paging(1,100); // grabs 100 tweets per page
+        Paging page = new Paging(1, 20); // grabs max 20 per page
         int p = 1;
-        while (p<=10){
+        while (p <= 3) {
             page.setPage(p);
-            statuses.addAll(twitter.getUserTimeline(handle));
+            statuses.addAll(twitter.getUserTimeline(handle,page));
             //retweets.addAll(twitter.getRetweetsOfMe());
             p++;
         }
+    }
+
+    /**
+     * Another helper method. This one gets the user's followers
+     */
+    private void getNetwork(String handle) throws TwitterException, IOException {
+        follows.add(twitter.getFriendsIDs(handle, -1));
+        followers.add(twitter.getFollowersIDs(handle, -1));
     }
 
 }
